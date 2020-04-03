6c4a5739cd002bc869378f7eb89df321066f091e
soohoonlee <jjinimania@gmail.com>
2020-04-03 12:08:36 +0900

    스프링 부트 설정 - JPA, DB

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -7,12 +7,14 @@ import javax.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.entity.Hello;
 import study.querydsl.entity.QHello;
 
 @SpringBootTest
 @Transactional
+@Commit
 class QuerydslApplicationTests {
 
   @Autowired
