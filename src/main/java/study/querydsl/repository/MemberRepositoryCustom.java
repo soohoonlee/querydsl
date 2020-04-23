b16d4ebab6a7810455564f8b3a71bda24e6592e1
soohoonlee <jjinimania@gmail.com>
2020-04-23 14:20:45 +0900

    스프링 데이터 페이징 활용1 - Querydsl 페이징 연동

--- src/main/java/study/querydsl/repository/MemberRepositoryCustom.java
+++ src/main/java/study/querydsl/repository/MemberRepositoryCustom.java
@@ -1,10 +1,16 @@
 package study.querydsl.repository;
 
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 
 public interface MemberRepositoryCustom {
 
   List<MemberTeamDto> search(MemberSearchCondition condition);
+
+  Page<MemberTeamDto> searchPagingSimple(MemberSearchCondition condition, Pageable pageable);
+
+  Page<MemberTeamDto> searchPagingComplex(MemberSearchCondition condition, Pageable pageable);
 }
